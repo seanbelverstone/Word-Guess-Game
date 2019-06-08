854a9ffdcd657affba35459617e2cb4ec89b3e5e
Sean Belverstone <seanbelverstone@gmail.com>
2019-06-07 19:53:32 -0500

    Added array word list

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,5 @@
 // Array of words
+var wordList = ["big ben", "queen elizabeth", "james bond", "football", "full english breakfast", 
+"cricket", "sherlock holmes", "stonehenge", "union jack", "the beatles", "fish and chips", "shakespeare",
+"kilts", "ireland", "scotland", "wales", "england"];
+
