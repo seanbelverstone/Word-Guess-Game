656bb324f391342fb52d637213f3c8f887d7edb2
Sean Belverstone <seanbelverstone@gmail.com>
2019-06-07 20:32:24 -0500

    Added variables for all the buttons and interactive section in the previously stated paragraph.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,3 +3,13 @@ var wordList = ["big ben", "queen elizabeth", "james bond", "football", "full en
 "cricket", "sherlock holmes", "stonehenge", "union jack", "the beatles", "fish and chips", "shakespeare",
 "kilts", "ireland", "scotland", "wales", "england"];
 
+var begin = document.getElementById("start");
+var wins = 0;
+var currentWord = document.getElementById("randomWord");
+var guessesLeft = 12;
+var lettersGuessed = document.getElementById("wrongLetter")
+
+//User input code
+document.onkeyup = function(event) {
+    var userInput = event.key;
+}
