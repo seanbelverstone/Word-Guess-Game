f6dd5c12e0ea1b0e698f4f6c5d7e3f9ff1147ba4
Sean Belverstone <seanbelverstone@gmail.com>
2019-06-08 12:29:19 -0500

    Added two functions, setWins and setGuesses. When any key is pressed, the wins and guesses left counters are set to their initial values.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,14 +4,28 @@ var wordList = ["big ben", "queen elizabeth", "james bond", "football", "full en
 "kilts", "ireland", "scotland", "wales", "england"];
 
 var startGame = document.getElementById("begin");
-var wins = 0;
+var wins = 0
 var currentWord = wordList[Math.floor(Math.random() * wordList.length)];;
 var guessesLeft = 12;
 var lettersGuessed = document.getElementById("wrongLetter")
 
+function setWins() {
+    winsLocation = document.getElementById("wins");
+    winsLocation.innerHTML = wins;
+}
+
+function setGuesses() {
+    guessesLocation = document.getElementById("guessesLeft");
+    guessesLocation.innerHTML = guessesLeft;
+}
+
+
 //User input code
 document.onkeyup = function(event) {
     var keyPressed = event.key.toLowerCase();
-    console.log("You pressed: " + keyPressed);
+    console.log("You pressed " + keyPressed);
+    setWins();
+    setGuesses();
+
 }
     
