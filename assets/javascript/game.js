9a8bc5d0e1b37d0270a00c327b6ececb882a2592
Sean Belverstone <seanbelverstone@gmail.com>
2019-06-12 16:43:27 -0500

    Changed image class to img id on html page. Created an if statement that checks the index of the word generated, and should append an image. Not loading the right image right now so will see what amendments i can make

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,7 +11,8 @@ var guessesLeft = 12;
 var wrongLetters = [];
 // var wordLetters = Array.from(currentWord);
 var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w",
-"x","y","z"];
+"x","y","z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
+"T", "U", "V", "W", "X", "Y", "Z"];
 
 // Setting functions for start of game
 
@@ -48,16 +49,201 @@ console.log(currentWord);
 
     var indexPosition = currentWordSplit.indexOf(keyPressed);
 
-    for (var i = 0; i < currentWordSplit.length; i++) {
-    }   if (indexPosition !== -1) {
+    for (var i = 0; i < currentWordSplit.length; i++) { //check if letter is valid
+        if (alphabet.indexOf(keyPressed, i) === -1) {
+            alert("try a real letter");
+            break;
+        } else if (indexPosition !== -1) {
             console.log("nice");
-    }   else {
+            currentWord.replace(ghostWord, currentWordSplit, [i]);
+            break;
+        } else { 
             console.log("try again");
             guessesLeft--;
             wrongLetters.push(keyPressed);
             console.log(wrongLetters);
             document.getElementById("lettersGuessed").innerHTML = wrongLetters; //Prints the wrong letters to the screen
-    }
+            break;
+        }
+           
+}
 
 }
 
+if (wordList.indexOf(0)) {
+    var img = new Image(400, 300);
+    var div = document.getElementById('img');
+ 
+    img.onload = function() {
+        div.appendChild(img);
+    };
+    img.src = 'assets/images/bigben.jpg';
+
+} else if (wordList.indexOf(1)) {
+    var img = new Image(400, 300);
+    var div = document.getElementById('img');
+
+    img.onload = function() {
+        div.appendChild(img);
+    };
+    img.src = 'assets/images/queenelizabeth.jpg';
+
+} else if (wordList.indexOf(2)) {
+    var img = new Image(400, 300);
+    var div = document.getElementById('img');
+
+    img.onload = function() {
+        div.appendChild(img);
+    };
+    img.src = 'assets/images/jamesbond.jpg';
+
+} else if (wordList.indexOf(3)) {
+    var img = new Image(400, 300);
+    var div = document.getElementById('img');
+
+    img.onload = function() {
+        div.appendChild(img);
+    };
+    img.src = 'assets/images/football.jpg';
+
+} else if (wordList.indexOf(4)) {
+    var img = new Image(400, 300);
+    var div = document.getElementById('img');
+
+    img.onload = function() {
+        div.appendChild(img);
+    };
+    img.src = 'assets/images/fullenglish.jpg';
+
+} else if (wordList.indexOf(5)) {
+    var img = new Image(400, 300);
+    var div = document.getElementById('img');
+
+    img.onload = function() {
+        div.appendChild(img);
+    };
+    img.src = 'assets/images/cricket.jpg';
+
+} else if (wordList.indexOf(6)) {
+    var img = new Image(400, 300);
+    var div = document.getElementById('img');
+
+    img.onload = function() {
+        div.appendChild(img);
+    };
+    img.src = 'assets/images/sherlockholmes.jpg';
+
+} else if (wordList.indexOf(7)) {
+    var img = new Image(400, 300);
+    var div = document.getElementById('img');
+
+    img.onload = function() {
+        div.appendChild(img);
+    };
+    img.src = 'assets/images/stonehenge.jpg';
+
+} else if (wordList.indexOf(8)) {
+    var img = new Image(400, 300);
+    var div = document.getElementById('img');
+
+    img.onload = function() {
+        div.appendChild(img);
+    };
+    img.src = 'assets/images/unionjack.jpg';
+
+} else if (wordList.indexOf(9)) {
+    var img = new Image(400, 300);
+    var div = document.getElementById('img');
+
+    img.onload = function() {
+        div.appendChild(img);
+    };
+    img.src = 'assets/images/thebeatles.jpg';
+
+} else if (wordList.indexOf(10)) {
+    var img = new Image(400, 300);
+    var div = document.getElementById('img');
+
+    img.onload = function() {
+        div.appendChild(img);
+    };
+    img.src = 'assets/images/fishandchips.jpg';
+
+} else if (wordList.indexOf(11)) {
+    var img = new Image(400, 300);
+    var div = document.getElementById('img');
+
+    img.onload = function() {
+        div.appendChild(img);
+    };
+    img.src = 'assets/images/shakespeare.jpg';
+
+} else if (wordList.indexOf(12)) {
+    var img = new Image(400, 300);
+    var div = document.getElementById('img');
+
+    img.onload = function() {
+        div.appendChild(img);
+    };
+    img.src = 'assets/images/kilts.jpg';
+
+} else if (wordList.indexOf(13)) {
+    var img = new Image(400, 300);
+    var div = document.getElementById('img');
+
+    img.onload = function() {
+        div.appendChild(img);
+    };
+    img.src = 'assets/images/ireland.png';
+
+} else if (wordList.indexOf(14)) {
+    var img = new Image(400, 300);
+    var div = document.getElementById('img');
+
+    img.onload = function() {
+        div.appendChild(img);
+    };
+    img.src = 'assets/images/scotland.png';
+
+} else if (wordList.indexOf(15)) {
+    var img = new Image(400, 300);
+    var div = document.getElementById('img');
+
+    img.onload = function() {
+        div.appendChild(img);
+    };
+    img.src = 'assets/images/wales.png';
+
+} else if (wordList.indexOf(16)) {
+    var img = new Image(400, 300);
+    var div = document.getElementById('img');
+
+    img.onload = function() {
+        div.appendChild(img);
+    };
+    img.src = 'assets/images/england.png';
+
+} else if (wordList.indexOf(17)) {
+    var img = new Image(400, 300);
+    var div = document.getElementById('img');
+
+    img.onload = function() {
+        div.appendChild(img);
+    };
+    img.src = 'assets/images/scone.jpeg';
+
+} else if (wordList.indexOf(18)) {
+    var img = new Image(400, 300);
+    var div = document.getElementById('img');
+
+    img.onload = function() {
+        div.appendChild(img);
+    };
+    img.src = 'assets/images/thames';
+    
+}
+/* things to add
+-replace _ with characters
+-allowed characters (a-z) - if incorrect character bring up alert. Disallow duplicate letters
+-when word complete - add image to page
+-bring up button(?) to continue, add function to button.. newdiv? append ect */ 
